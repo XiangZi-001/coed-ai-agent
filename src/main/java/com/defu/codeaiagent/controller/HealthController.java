29d336aa9b6a0ccb8d0acc2d9565e5c9b0b92d0e
SH W <437861513@qq.com>
2025-06-15 18:15:30 +0800

    modify code

--- src/main/java/com/defu/codeaiagent/controller/HealthController.java
+++ src/main/java/com/defu/codeaiagent/controller/HealthController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/health")
 public class HealthController {
 
+    /**
+     * 测试 knife4j 是否引入成功
+     * @return
+     */
     @GetMapping
     public String healthController(){
         return "OK";
