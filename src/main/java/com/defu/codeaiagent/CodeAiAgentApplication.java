a443530c500d21fa3c91a560109e7af1f2e73183
SH W <437861513@qq.com>
2025-06-15 18:08:57 +0800

    modify code

---
+++ src/main/java/com/defu/codeaiagent/CodeAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.defu.codeaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/defu/codeaiagent/CodeAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.defu.codeaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
